89efc3898b459c966170c07c0f7ed68e40b146d3
Bret Mda <146015182+br3tdev@users.noreply.github.com>
2025-12-04 10:44:21 +0300

    Db setup (#1)
    * Set up db w/ Prisma ORM
    
    * Fix vercel build command
    
    ---------
    
    Co-authored-by: bret <bretai@LCARS-MX-23>

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,28 @@
 import express from "express";
 
+import { prisma } from "../../lib/db"
+
 import type MessageResponse from "../interfaces/message-response.js";
 
 import emojis from "./emojis.js";
 
 const router = express.Router();
 
-router.get<object, MessageResponse>("/", (req, res) => {
-  res.json({
-    message: "API - 👋🌎🌍🌏",
-  });
-});
+// router.get<object, MessageResponse>("/", (req, res) => {
+//   res.json({
+//     message: "API - 👋🌎🌍🌏",
+//   });
+// });
+
+router.get("/", async (req, res) => {
+  const customer = await prisma.customer.findFirst({
+    where: {
+      idNumber: "32518496"
+    }
+  })
+
+  res.json(customer)
+})
 
 router.use("/emojis", emojis);
 
